05eb8149a60f7fc4b44ab9367915ce751e68c7d3
luxopes <bambiliarda@gmail.com>
2026-09-21 13:17:27 +0200

    Improve input handling and game logic in game.py
    Refactor number guessing game to handle invalid inputs and reset attempts.

--- game.py
+++ game.py
@@ -1,17 +1,37 @@
 import random as r
 
-rand = r.randint(1, 100)
-user_raw = input("Typni si cislo od 0 do 100: ")
+a = r.randint(1, 100)
 
-user = int(user_raw)
+spatne = 0
 
-if rand < user:
-	print("Moc velke!")
-elif rand > user:
-	print("Moc male!")
-elif rand == user:
-	print("Super, mas bod")
-else:
-	print("Chyba! Zkuste to znovu")
+while True:
 
-print(f"Cislo bylo {rand}")
+    text = input("Your guess: ")
+
+    try:
+        b = int(text.strip())
+
+    except ValueError:
+
+        spatne += 1
+
+        if spatne >= 5:
+            print("Too many invalid inputs, ending.")
+            break
+
+        print("That is not a whole number, try again.")
+        continue
+
+    spatne = 0
+
+    if a < b:
+        print("Too big!")
+
+    elif a > b:
+        print("Too small!")
+
+    else:
+        print("Correct!")
+        break
+
+print(f"Correct number was {a}")
