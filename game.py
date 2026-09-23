86279c4026bdd7f2ebbd5388f6e3bc8f8bba0838
luxopes <bambiliarda@gmail.com>
2026-09-23 08:01:35 +0200

    Update game.py

--- game.py
+++ game.py
@@ -1,37 +1,40 @@
 import random as r
 
-secret_number = r.randint(1, 100)
-
-invalid_attempts = 0
-
 while True:
-
-    user_input = input("Your guess: ")
-
-    try:
-        guess = int(user_input.strip())
-
-    except ValueError:
-
-        invalid_attempts += 1
-
-        if invalid_attempts >= 5:
-            print("Too many invalid inputs, ending.")
+    secret = r.randint(1, 100)
+    attempts = 0
+    
+    print("\nI picked a number between 1 and 100.")
+    
+    while True:
+        user_input = input("Your turn: ")
+        
+        try:
+            guess = int(user_input)
+            
+            if guess < 1 or guess > 100:
+                print("Please enter a whole number between 1 and 100")
+                continue
+            
+        except ValueError:
+            print("That is not a whole number, try again")
+            continue
+            
+        attempts += 1
+        
+        if secret > guess:
+            print("Too small!")
+            
+        elif secret < guess:
+            print("Too big!")
+            
+        else:
+            print("Congratulations!")
+            print(f"You guessed the number in {attempts} attempts.")
             break
-
-        print("That is not a whole number, try again.")
-        continue
-
-    invalid_attempts = 0
-
-    if secret_number < guess:
-        print("Too big!")
-
-    elif secret_number > guess:
-        print("Too small!")
-
-    else:
-        print("Correct!")
+            
+    play_again = input("Do you want to play again? (yes/no): ").strip().lower()
+        
+    if play_again not in ("yes", "y"):
+        print("Thanks for playing!")
         break
-
-print(f"Correct number was {secret_number}")
